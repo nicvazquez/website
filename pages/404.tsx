053b2cb7f64ede9a34cf4b0fdef10249c5f19c82
Nicolás Vázquez <nvazquez@novared.net>
2022-07-13 11:07:31 -0300

    404 page added

---
+++ pages/404.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import React from "react";
+import Link from "next/link";
+
+const Page404: NextPage = () => {
+	return (
+		<div className="container">
+			<div>
+				<h1>404 | Página no encontrada</h1>
+				<Link href="/">
+					<button>Volver al inicio</button>
+				</Link>
+			</div>
+			{/* @ts-ignore */}
+			<style jsx>{
+				/*css*/ `
+					.container {
+						height: 100vh;
+						width: 100%;
+						display: grid;
+						place-items: center;
+						text-align: center;
+					}
+					.container div {
+						display: flex;
+						flex-direction: column;
+						align-items: center;
+					}
+					button {
+						margin-top: 2rem;
+						padding: 0.5rem 1rem;
+						border: none;
+						border-radius: 5px;
+						background-color: var(--primary);
+						color: #fafafa;
+						cursor: pointer;
+					}
+				`
+			}</style>
+		</div>
+	);
+};
+
+export default Page404;
